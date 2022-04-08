a858ded9691645f502290611002f631596e2a031
JTech_Office_PC\wuxubaiyang <wuxubaiyang@gmail.com>
2022-04-08 17:41:43 +0800

    +1

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -3,93 +3,48 @@ import 'package:jtech_pomelo/pomelo.dart';
 
 void main() {
   runJAppRoot(
-    title: "pomelo demo",
-    pageLoad: () async {
-      return const MyHomePage(title: "pomelo demo");
-    },
-    debug: false,
+    title: "PomeloDemo",
+    debug: true,
+    homePage: const WelcomePage(),
   );
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
+/*
+* 欢迎页面
+* @author JTech JH
+* @Time 2022/4/8 17:31
+*/
+class WelcomePage extends BaseStatefulWidget {
+  const WelcomePage({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<StatefulWidget> createState() => _WelcomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  void initState() {
-    super.initState();
-  }
-
+/*
+* 欢迎页面-状态
+* @author JTech JH
+* @Time 2022/4/8 17:31
+*/
+class _WelcomePageState extends BaseState<WelcomePage> {
   @override
   Widget build(BuildContext context) {
     return JAppPage(
-      title: Text(widget.title),
+      showAppbar: false,
       body: Center(
-        child: ElevatedButton(
-          child: Text("click"),
-          onPressed: () {
-            JUpdateUtil.checkByPhoneQ(appName: "yq_mobile");
-          },
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Icon(
+              Icons.flutter_dash_rounded,
+              color: JUtil.getAccentColor(context),
+              size: 65,
+            ),
+            const SizedBox(height: 15),
+            const Text("Pomelo Demo"),
+          ],
         ),
       ),
-      // body: JForm(
-      //   formBuilder: (context, controller) {
-      //     return Column(
-      //       children: [
-      //         JFormFieldText(
-      //           id: "name",
-      //           controller: controller,
-      //           initialValue: "aaaa",
-      //           targetId: "date",
-      //           targetChange: (f, value) {
-      //             f?.didChange(JDateUtil.formatDate("yyyy-mm-dd", value));
-      //           },
-      //         ),
-      //         JFormFieldDatePicker.dateTime(
-      //           id: "date",
-      //           controller: controller,
-      //           valueChange: (v) {
-      //             print(v);
-      //           },
-      //         ),
-      //         JFormFieldSelector(
-      //           id: "aa",
-      //           controller: controller,
-      //           items: [
-      //             SelectItem.same(text: "aa"),
-      //             SelectItem.same(text: "bb"),
-      //           ],
-      //         ),
-      //         JFormFieldFile(
-      //           id: "gg",
-      //           controller: controller,
-      //           pickerItems: [
-      //             PickerMenuItem.image(text: "aaa"),
-      //             PickerMenuItem.imageTake(text: "bbb"),
-      //           ],
-      //           fileUpload: (List<JFile> files, value) async {
-      //             return FileUploadResult(value: value, files: files);
-      //           },
-      //           fileLoad: (Object? value) async {
-      //             return [];
-      //           },
-      //         ),
-      //       ],
-      //     );
-      //   },
-      // ),
     );
   }
-}
-
-class City extends BaseIndexModel {
-  final String name;
-
-  City(this.name) : super.create(tag: name);
 }
